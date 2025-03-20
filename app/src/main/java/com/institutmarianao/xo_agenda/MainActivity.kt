b3e5593a5233e533c5850405936e750afba3d9a2
xavki <xavimb04@gmail.com>
2025-03-20 20:55:34 +0100

    base de dades

--- app/src/main/java/com/institutmarianao/xo_agenda/MainActivity.kt
+++ app/src/main/java/com/institutmarianao/xo_agenda/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.institutmarianao.xo_agenda
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val db = Firebase.firestore
+        //val db = FirebaseFirestore.getInstance()
+
     }
 
     //MENSAJE DE PRUEBA GITHUB.
