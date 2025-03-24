33a23f2b5de904d9ceff4428a9c3b364fe28cbb0
joam6 <joseporiolam@gmail.com>
2025-03-24 20:46:37 +0100

    Iconos APP

--- app/src/main/java/com/institutmarianao/xo_agenda/MainActivity.kt
+++ app/src/main/java/com/institutmarianao/xo_agenda/MainActivity.kt
@@ -2,7 +2,6 @@ package com.institutmarianao.xo_agenda
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -10,7 +9,7 @@ import com.google.firebase.ktx.Firebase
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         val db = Firebase.firestore
         //val db = FirebaseFirestore.getInstance()
 
