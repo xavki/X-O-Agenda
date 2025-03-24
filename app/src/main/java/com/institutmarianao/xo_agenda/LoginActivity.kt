5e521b94fecdab6c2c1759e7619a2313ed881c22
xavki <xavimb04@gmail.com>
2025-03-24 20:20:53 +0100

    login y dependencias

---
+++ app/src/main/java/com/institutmarianao/xo_agenda/LoginActivity.kt
@@ -0,0 +1,32 @@
+package com.institutmarianao.xo_agenda
+package com.institutmarianao.xo_agenda
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+
+        val email = "prueba@correo.com"
+        val password = "123456"
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    println("Usuario registrado: ${user?.email}")
+                } else {
+                    println("Error: ${task.exception}")
+                }
+            }
+    }
+}
