b6af8fc6fed40abb6f232f489faaa5af8ba48fd0
xavki <xavimb04@gmail.com>
2025-03-28 17:47:48 +0100

    menu faltaria poner los datos pero esta hecho

--- app/src/main/java/com/institutmarianao/xo_agenda/HomeActivity.kt
+++ app/src/main/java/com/institutmarianao/xo_agenda/HomeActivity.kt
@@ -1,11 +1,17 @@
 package com.institutmarianao.xo_agenda
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.institutmarianao.xo_agenda.adapters.MenuAdapter
 
 
 // Pantalla principal con calendario y tareas
-class HomeActivity : AppCompatActivity() {
+/*class HomeActivity : AppCompatActivity() {
 
     //private lateinit var auth: FirebaseAuth
 
@@ -18,4 +24,36 @@ class HomeActivity : AppCompatActivity() {
 
 
     }
+}*/
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var recyclerView: RecyclerView
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawer_menu)
+        recyclerView = findViewById(R.id.recycler_view_menu)
+
+        // Cargar el fragment del menú
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container_fragment, MenuFragment())
+                .commit()
+        }
+
+        // Configurar el RecyclerView con el adaptador
+        val menuItems = listOf("Opción 1", "Opción 2", "Opción 3")
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = MenuAdapter(menuItems)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Aquí puedes manejar las opciones del menú
+        return super.onOptionsItemSelected(item)
+    }
 }
+
