f4b89f866689b68ca99dd4a9de6d66df340268dd
xavki <xavimb04@gmail.com>
2025-05-15 20:45:38 +0200

    notificacion

--- app/src/main/java/com/institutmarianao/xo_agenda/ReminderReceiver.kt
+++ app/src/main/java/com/institutmarianao/xo_agenda/ReminderReceiver.kt
@@ -1,38 +1,92 @@
 package com.institutmarianao.xo_agenda
 
+import android.app.Notification.EXTRA_NOTIFICATION_ID
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.provider.Settings.System.getString
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat.getSystemService
+import kotlin.coroutines.jvm.internal.CompletedContinuation.context
+
 
 class ReminderReceiver: BroadcastReceiver() {
+
+    companion object {
+        const val channelId = "XO_AGENDA_CHANNEL"
+    }
+
+
+
     override fun onReceive(ctx: Context, intent: Intent) {
         val title = intent.getStringExtra("title") ?: "Recordatori"
         val desc  = intent.getStringExtra("desc")  ?: ""
-        val channelId = "XO_AGENDA_CHANNEL"
 
-        // 1) Crear canal (solo una vez es suficiente)
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            val chan = NotificationChannel(
-                channelId,
-                "Alertes de l’Agenda",
-                NotificationManager.IMPORTANCE_HIGH
-            ).apply { description = "Canal per als recordatoris" }
-            ctx.getSystemService(NotificationManager::class.java)
-                .createNotificationChannel(chan)
-        }
+        createNotificationChannel(context)
 
-        // 2) Construir i mostrar la notificació
+        // Construir i mostrar la notificació
         val notif = NotificationCompat.Builder(ctx, channelId)
             .setSmallIcon(R.drawable.logoxajo)  // tu icona
             .setContentTitle(title)
             .setContentText(desc)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setAutoCancel(true)
             .build()
 
         ctx.getSystemService(NotificationManager::class.java)
             .notify(title.hashCode(), notif)
+
+        // Create an explicit intent for an Activity in your app.
+        val intent = Intent(this, AlertDetails::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        val builder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.logoxajo)
+            .setContentTitle("My notification")
+            .setContentText("Hello World!")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            // Set the intent that fires when the user taps the notification.
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        val ACTION_SNOOZE = "posponer"
+
+        val snoozeIntent = Intent(this, MyBroadcastReceiver::class.java).apply {
+            action = ACTION_SNOOZE
+            putExtra(EXTRA_NOTIFICATION_ID, 0)
+        }
+        val snoozePendingIntent: PendingIntent =
+            PendingIntent.getBroadcast(this, 0, snoozeIntent, 0)
+        val builder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.logoxajo)
+            .setContentTitle("My notification")
+            .setContentText("Hello World!")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .addAction(getString(R.string.snooze),
+                snoozePendingIntent)
+
+
     }
+    fun createNotificationChannel(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = context.getString(R.string.channel_name)
+            val descriptionText = context.getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_HIGH           // alta prioridad
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+
 }
